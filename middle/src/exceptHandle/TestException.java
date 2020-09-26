1fa0e49fdc4ec86473b9d3b1fbaa913b28edb299
lingfeng23 <15131257162@163.com>
2020-09-26 23:01:26 +0800

    异常处理

--- middle/src/exceptHandle/TestException.java
+++ middle/src/exceptHandle/TestException.java
@@ -2,6 +2,7 @@ package exceptHandle;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 /**
  * @author malf
@@ -14,6 +15,12 @@ public class TestException {
 		File file = new File("D:/LOL.txt");
 
 		// 试图打开文件LOL.txt，会抛出FileNotFoundException，如果不处理该异常，就会有编译错误
-		//new FileInputStream(file);
+		try {
+			new FileInputStream(file);
+		} catch (FileNotFoundException e) {
+
+		} finally {
+			System.out.println("呵呵哒");
+		}
 	}
 }
--- middle/src/middle.md
+++
@@ -1,3 +0,0 @@
-###《Java 中级》
-####异常处理（exceptHandle）
-
