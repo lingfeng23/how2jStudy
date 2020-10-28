3c9a0d67224184a3d80789d1082f01afd43ba9cd
lingfeng23 <15131257162@163.com>
2020-10-28 14:23:24 +0800

    实战项目-天猫(SpringBoot版本)初始化

---
+++ pro_tmall/src/main/java/com/malf/ProTmallApplication.java
@@ -0,0 +1,13 @@
+package com.malf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProTmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProTmallApplication.class, args);
+	}
+
+}
