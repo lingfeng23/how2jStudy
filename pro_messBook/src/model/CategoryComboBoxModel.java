400a45e767bea4158b4110febdbf6089e90e0de4
lingfeng23 <15131257162@163.com>
2020-11-03 14:42:02 +0800

    RecordPanel

--- pro_messBook/src/model/CategoryComboBoxModel.java
+++ pro_messBook/src/model/CategoryComboBoxModel.java
@@ -1,10 +1,55 @@
 package model;
 
+import javax.swing.*;
+import javax.swing.event.ListDataListener;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author malf
  * @description 分类下拉框Model类
  * @project how2jStudy
  * @since 2020/11/2
  */
-public class CategoryComboBoxModel {
+public class CategoryComboBoxModel implements ComboBoxModel<String> {
+	public List<String> categories = new ArrayList<>();
+	String category;
+
+	public CategoryComboBoxModel() {
+		categories.add("餐饮");
+		categories.add("交通");
+		categories.add("话费");
+		categories.add("住宿");
+		category = categories.get(0);
+	}
+
+	@Override
+	public void setSelectedItem(Object anItem) {
+		category = (String) anItem;
+	}
+
+	@Override
+	public Object getSelectedItem() {
+		return category;
+	}
+
+	@Override
+	public int getSize() {
+		return categories.size();
+	}
+
+	@Override
+	public String getElementAt(int index) {
+		return null;
+	}
+
+	@Override
+	public void addListDataListener(ListDataListener l) {
+
+	}
+
+	@Override
+	public void removeListDataListener(ListDataListener l) {
+
+	}
 }
--- pro_messBook/src/panel/RecordPanel.java
+++ pro_messBook/src/panel/RecordPanel.java
@@ -1,10 +1,63 @@
 package panel;
 
+import model.CategoryComboBoxModel;
+import org.jdesktop.swingx.JXDatePicker;
+import util.ColorUtil;
+import util.GUIUtil;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Date;
+
 /**
  * @author malf
  * @description 记一笔面板类
  * @project how2jStudy
  * @since 2020/11/2
  */
-public class RecordPanel {
+public class RecordPanel extends JPanel {
+	static {
+		GUIUtil.useLNF();
+	}
+
+	public static RecordPanel instance = new RecordPanel();
+
+	JLabel spend = new JLabel("花费(￥)");
+	JLabel category = new JLabel("分类");
+	JLabel comment = new JLabel("备注");
+	JLabel date = new JLabel("日期");
+
+	public JTextField textSpend = new JTextField("0");
+	public CategoryComboBoxModel boxModel = new CategoryComboBoxModel();
+	public JComboBox<String> boxCategory = new JComboBox<>(boxModel);
+	public JTextField textComment = new JTextField();
+	public JXDatePicker datePicker = new JXDatePicker(new Date());
+	JButton button = new JButton("记一笔");
+
+	public RecordPanel() {
+		GUIUtil.setColor(ColorUtil.GRAY, spend, category, comment, date);
+		GUIUtil.setColor(ColorUtil.BLUE, button);
+		JPanel pInput = new JPanel();
+		JPanel pSubmit = new JPanel();
+		int gap = 40;
+		pInput.setLayout(new GridLayout(4, 2, gap, gap));
+
+		pInput.add(spend);
+		pInput.add(textSpend);
+		pInput.add(category);
+		pInput.add(boxCategory);
+		pInput.add(comment);
+		pInput.add(textComment);
+		pInput.add(date);
+		pInput.add(datePicker);
+		pSubmit.add(button);
+
+		this.setLayout(new BorderLayout());
+		this.add(pInput, BorderLayout.NORTH);
+		this.add(pSubmit, BorderLayout.CENTER);
+	}
+
+	public static void main(String[] args) {
+		GUIUtil.showPanel(RecordPanel.instance);
+	}
 }
