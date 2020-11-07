e39fb9cc8f9e9b4046b612680e19d551adbb0df2
lingfeng23 <15131257162@163.com>
2020-11-07 11:46:26 +0800

    SpringCloud：断路器聚合监控(DONE)

---
+++ distributed/parent/turbine/src/main/java/com/malf/ProductServiceTurbineApplication.java
@@ -0,0 +1,26 @@
+package com.malf;
+
+import cn.hutool.core.util.NetUtil;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+/**
+ * @author malf
+ * @description TODO
+ * @project parent
+ * @since 2020/11/7
+ */
+@SpringBootApplication
+@EnableTurbine
+public class ProductServiceTurbineApplication {
+	public static void main(String[] args) {
+		int port = 8021;
+		if (!NetUtil.isUsableLocalPort(port)) {
+			System.err.printf("端口%d被占用了，无法启动%n", port);
+			System.exit(1);
+		}
+		new SpringApplicationBuilder(ProductServiceTurbineApplication.class).properties("server.port=" + port).run(args);
+
+	}
+}
